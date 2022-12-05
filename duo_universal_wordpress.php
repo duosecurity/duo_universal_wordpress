1294c5490ee0a2db7db447adc6024c69d2df2b8e
Mark Bishop <mbishop@duosecurity.com>
2022-12-05 11:38:47 -0500

    Add helpers for dealing with user authentication transients

--- duo_universal_wordpress.php
+++ duo_universal_wordpress.php
@@ -36,8 +36,34 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     $GLOBALS['DuoSecAuthCookieName'] = 'duo_secure_wordpress_auth_cookie';
     $GLOBALS['DuoDebug'] = false;
 
+    // expire in 48hrs
+    const DUO_TRANSIENT_EXPIRATION = 48*60*60;
+
+    // Sets a user's auth state
+    // user: username of the user to update
+    // status: most recent phase of authentication that has been completed ("primary", "secondary" or "authenticated")
+    function update_user_auth_status($user, $status, $oidc_state=NULL) {
+        set_transient("duo_auth_".$user."_status", $status, DUO_TRANSIENT_EXPIRATION);
+        set_transient("duo_auth_".$user."_oidc_state", $oidc_state, DUO_TRANSIENT_EXPIRATION);
+    }
+
+    function clear_current_user_auth($user) {
+        $user = wp_get_current_user();
+        // technically these could fail. What do?
+        delete_transient("duo_auth_".$user->login_status."_status");
+        delete_transient("duo_auth_".$user->login_status."_oidc_state");
+    }
+
     function duo_start_second_factor($user, $redirect_to=NULL){
-        // TODO
+        // TODO not sure if we need this redirect
+        if (!$redirect_to){
+            // Some custom themes do not provide the redirect_to value
+            // Admin page is a good default
+            $redirect_to = isset( $_POST['redirect_to'] ) ? $_POST['redirect_to'] : admin_url();
+        }
+
+        wp_logout();
+        exit();
     }
     
     function duo_authenticate_user($user="", $username="", $password="") {
@@ -46,7 +72,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
             return $user;
         }
 
-        if (! duo_auth_enabled()){
+        if (!duo_auth_enabled()){
             duo_debug_log('Duo not enabled, skipping 2FA.');
             return;
         }
@@ -65,6 +91,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
             }
             if(!duo_role_require_mfa($user)){
                 duo_debug_log("Skipping 2FA for user: $username with roles: " . print_r($user->roles, true));
+                update_user_auth_status($user->user_login, "authenticated");
                 return;
             }
 
@@ -75,62 +102,43 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
                 return $user;
             } else {
                 duo_debug_log("Primary auth succeeded, starting second factor for $username");
+                update_user_auth_status($user->user_login, "primary");
                 duo_start_second_factor($user);
             }
         }
         duo_debug_log('Starting primary authentication');
     }
 
-
-    // TODO we may or may not be using cookie verification in the final version of this
-    // the body of this function might change considerably.
     function duo_verify_auth(){
     /*
         Verify the user is authenticated with Duo. Start 2FA otherwise
     */
         if (! duo_auth_enabled()){
+            // TODO do we still need this skipping logic?
             if (is_multisite()) {
                 $site_info = get_current_site();
                 duo_debug_log("Duo not enabled on " . $site_info->site_name . ', skip cookie check.');
             }
             else {
-                duo_debug_log('Duo not enabled, skip cookie check.');
+                duo_debug_log('Duo not enabled, skip auth check.');
             }
             return;
         }
 
         if(is_user_logged_in()){
             $user = wp_get_current_user();
-            duo_debug_log("Verifying second factor for user: $user->user_login URL: " .  duo_get_uri() . ' path: ' . COOKIEPATH . ' network path: ' . SITECOOKIEPATH . 'cookie domain: ' . COOKIE_DOMAIN . ' is SSL: ' . is_ssl());
-            if (duo_role_require_mfa($user) and !duo_verify_cookie($user)){
-                duo_debug_log("Duo cookie invalid for user: $user->user_login");
+            duo_debug_log("Verifying auth state for user: $user->user_login");
+            if (duo_role_require_mfa($user) and !duo_verify_auth_status($user->user_login)){
+                duo_debug_log("User not authenticated with Duo. Starting second factor for: $user->user_login");
                 duo_start_second_factor($user, duo_get_uri());
             }
             duo_debug_log("User $user->user_login allowed");
         }
     }
 
-    function duo_unset_cookie(){
-        global $DuoAuthCookieName;
-        global $DuoSecAuthCookieName;
-        setcookie($DuoAuthCookieName, '', strtotime('-1 day'), COOKIEPATH, COOKIE_DOMAIN);
-        setcookie($DuoAuthCookieName, '', strtotime('-1 day'), SITECOOKIEPATH, COOKIE_DOMAIN);
-        setcookie($DuoSecAuthCookieName, '', strtotime('-1 day'), COOKIEPATH, COOKIE_DOMAIN);
-        setcookie($DuoSecAuthCookieName, '', strtotime('-1 day'), SITECOOKIEPATH, COOKIE_DOMAIN);
-        duo_debug_log("Unset Duo cookie for path: " . COOKIEPATH . " network path: " . SITECOOKIEPATH . " on domain: " . COOKIE_DOMAIN);
-    }
-
-    if(!function_exists('hash_equals')) {
-      function hash_equals($str1, $str2) {
-        if(strlen($str1) != strlen($str2)) {
-          return false;
-        } else {
-          $res = $str1 ^ $str2;
-          $ret = 0;
-          for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
-          return !$ret;
-        }
-      }
+    function duo_verify_auth_status($user) {
+        $status = get_transient("duo_auth_".$user."_status");
+        return ($status == "authenticated");
     }
 
     /*-------------XML-RPC Features-----------------*/
@@ -143,7 +151,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
     add_action('init', 'duo_verify_auth', 10);
 
-    add_action('clear_auth_cookie', 'duo_unset_cookie', 10);
+    add_action('clear_auth_cookie', 'clear_current_user_auth', 10);
 
     add_filter('authenticate', 'duo_authenticate_user', 10, 3);
     
