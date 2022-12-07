31c52223bd6d0e6fff17edf39325f887e698c2c0
Mark Bishop <mbishop@duosecurity.com>
2022-12-07 12:06:46 -0500

    Add Duo Client initialization and fix helper functions
    Some of the helper functions ended up being less than helpful since we don't actually
    know the user's username after they get redirected from Duo back to wordpress. Consequently we
    can't lookup transients by username we actually need to use their OIDC state instead and map
    that to the username that it should be.

--- duo_universal_wordpress.php
+++ duo_universal_wordpress.php
@@ -30,28 +30,60 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
     require_once('duo_settings.php');
     require_once('utilities.php');
+    require_once('vendor/autoload.php');
+
+    use Duo\DuoUniversal\Client;
 
     # We may or may not even need cookies
     $GLOBALS['DuoAuthCookieName'] = 'duo_wordpress_auth_cookie';
     $GLOBALS['DuoSecAuthCookieName'] = 'duo_secure_wordpress_auth_cookie';
     $GLOBALS['DuoDebug'] = false;
+    $DuoClient = NULL;
 
     // expire in 48hrs
     const DUO_TRANSIENT_EXPIRATION = 48*60*60;
 
     // Sets a user's auth state
     // user: username of the user to update
-    // status: most recent phase of authentication that has been completed ("primary", "secondary" or "authenticated")
-    function update_user_auth_status($user, $status, $oidc_state=NULL) {
+    // status: whether or not an authentication is in progress or is completed ("in-progress" or "authenticated")
+    function update_user_auth_status($user, $status, $redirect_url="", $oidc_state=NULL) {
         set_transient("duo_auth_".$user."_status", $status, DUO_TRANSIENT_EXPIRATION);
-        set_transient("duo_auth_".$user."_oidc_state", $oidc_state, DUO_TRANSIENT_EXPIRATION);
+        if($redirect_url) {
+            set_transient("duo_auth_".$user."_redirect_url", $redirect_url, DUO_TRANSIENT_EXPIRATION);
+        }
+        if($oidc_state) {
+            // we need to track the state in two places so we can clean up later
+            set_transient("duo_auth_".$user."_oidc_state", $oidc_state, DUO_TRANSIENT_EXPIRATION);
+            set_transient("duo_auth_state_$oidc_state", $user, DUO_TRANSIENT_EXPIRATION);
+        }
+    }
+
+    function get_user_auth_status($user) {
+        return get_transient("duo_auth_".$user."_status");
+    }
+
+    function get_username_from_oidc_state($oidc_state) {
+        return get_transient("duo_auth_state_$oidc_state");
+    }
+
+    function get_redirect_url($user) {
+        return get_transient("duo_auth_".$user."_redirect_url");
     }
 
     function clear_current_user_auth($user) {
         $user = wp_get_current_user();
+        $username = $user->login_status;
+        $oidc_state = get_transient("duo_auth_".$username."_oidc_state");
         // technically these could fail. What do?
-        delete_transient("duo_auth_".$user->login_status."_status");
-        delete_transient("duo_auth_".$user->login_status."_oidc_state");
+        delete_transient("duo_auth_".$username."_status");
+        delete_transient("duo_auth_".$username."_oidc_state");
+        delete_transient("duo_auth_state_$oidc_state");
+        delete_transient("duo_auth_".$username."_redirect_url");
+    }
+
+    function get_page_url() {
+        $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        return $protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING'];
     }
 
     function duo_start_second_factor($user, $redirect_to=NULL){
@@ -114,7 +146,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
         Verify the user is authenticated with Duo. Start 2FA otherwise
     */
         if (! duo_auth_enabled()){
-            // TODO do we still need this skipping logic?
+            // XXX do we still need this skipping logic?
             if (is_multisite()) {
                 $site_info = get_current_site();
                 duo_debug_log("Duo not enabled on " . $site_info->site_name . ', skip cookie check.');
@@ -123,6 +155,14 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
                 duo_debug_log('Duo not enabled, skip auth check.');
             }
             return;
+        } else {
+            global $DuoClient;
+            $DuoClient = new Client(
+                duo_get_option('duo_ikey'),
+                duo_get_option('duo_skey'),
+                duo_get_option('duo_host'),
+                "",
+            );
         }
 
         if(is_user_logged_in()){
@@ -154,5 +194,5 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     add_action('clear_auth_cookie', 'clear_current_user_auth', 10);
 
     add_filter('authenticate', 'duo_authenticate_user', 10, 3);
-    
+
 ?>
