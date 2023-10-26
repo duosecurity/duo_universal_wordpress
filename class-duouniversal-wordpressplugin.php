53c425fa69e738c4634325513bc35adb827d8c5f
Mark Bishop <mbishop@duosecurity.com>
2023-10-26 16:22:06 -0400

    Align file names to classes (#19)

--- class-duouniversal-wordpressplugin.php
+++ class-duouniversal-wordpressplugin.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once 'class-duo-universal-settings.php';
-require_once 'class-duo-universal-utilities.php';
-require_once 'class-duo-universal-wordpress-helper.php';
+require_once 'class-duouniversal-settings.php';
+require_once 'class-duouniversal-utilities.php';
+require_once 'class-duouniversal-wordpresshelper.php';
 require_once 'vendor/autoload.php';
 
 use Duo\DuoUniversal\Client;
