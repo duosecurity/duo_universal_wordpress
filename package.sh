2bc108d88f1d771319efb3e5ee967f70c54d04f1
Mark Bishop <mbishop@duosecurity.com>
2023-11-08 09:19:42 -0500

    Add packaging tools

---
+++ package.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+zip -r duo-universal.zip duo-universal/LICENSE duo-universal/README.md duo-universal/composer.json duo-universal/composer.lock duo-universal/*php duo-universal/vendor duo-universal/readme.txt
