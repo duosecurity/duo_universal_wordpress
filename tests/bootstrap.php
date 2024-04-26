84f7352a3f821b9db66bec0990b3e01447cef7c6
Michael Sumner <75835774+michael-sumner@users.noreply.github.com>
2024-04-26 08:07:06 +0100

    fix(bootstrap): remove duplicate / in path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // First we need to load the composer autoloader, so we can use WP Mock
-require_once plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
 // Bootstrap WP_Mock to initialize built-in features
 WP_Mock::bootstrap();
