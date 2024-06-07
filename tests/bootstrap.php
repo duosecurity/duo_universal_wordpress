917d0c6290e47341a3506d41b013da99494d5f7d
Jeffrey Parker <jeffrpar@cisco.com>
2024-06-07 10:15:05 -0400

    Fix unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+// "Mock" this wordpress function so unit tests can still use it
+function plugin_dir_path($file) {
+    return './';
+}
+
 // First we need to load the composer autoloader, so we can use WP Mock
 require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
